54ae18fdf2ddd6e3f755a0c093dc89574f083a3b
kavishkaHerath <hmravindu@gmail.com>
2024-02-13 09:24:28 +0530

    calculate 1st version

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        float annualInterestRate = scanner.nextFloat();
+        float monthlyInterest = annualInterestRate / MONTHS_IN_YEAR / PERCENT;
+
+        System.out.print("Period (Years): ");
+        byte years = scanner.nextByte();
+        int noOfPeriod = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal *
+                (monthlyInterest * Math.pow((1+monthlyInterest), noOfPeriod)
+                        / (Math.pow((1+monthlyInterest), noOfPeriod) - 1));
+        String result = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + result);
+    }
+}
