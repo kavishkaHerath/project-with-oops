50a69cc016b76112e1a74b2e5c213ed0fc7052da
kavishkaHerath <hmravindu@gmail.com>
2024-02-13 11:49:26 +0530

    input values validation

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,37 @@ public class Main {
     public static void main(String[] args) {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
+        int principal = 0;
+        float annualInterestRate = 0;
+        byte years = 0;
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        while (true){
+            System.out.print("Principal ($ 1k - $ 1M: ");
+            principal = scanner.nextInt();
+            if(principal >= 1000 && principal < 1_000_000)
+                break;
+            System.out.println("Enter a value between 1,000 and 1,000,000");
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterestRate = scanner.nextFloat();
+        while (true){
+            System.out.print("Annual Interest Rate: ");
+            annualInterestRate = scanner.nextFloat();
+            if(annualInterestRate > 0 && annualInterestRate <= 30)
+                break;
+            System.out.println("Enter a value greater than 0 and less than 30");
+        }
+
         float monthlyInterest = annualInterestRate / MONTHS_IN_YEAR / PERCENT;
 
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
+        while (true){
+            System.out.print("Period (Years): ");
+            years = scanner.nextByte();
+            if(years > 0 && years <= 30)
+                break;
+            System.out.println("Enter a value greater than 0 and less than 30");
+        }
         int noOfPeriod = years * MONTHS_IN_YEAR;
 
         double mortgage = principal *
