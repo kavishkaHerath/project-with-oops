3e3ab0e65139fa2083d721d54e628fb82cc0b900
kavishkaHerath <hmravindu@gmail.com>
2024-02-13 09:40:39 +0530

    calculate 2nd version

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,23 @@ public class Main {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
-        int result = MONTHS_IN_YEAR * PERCENT;
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.print("Principal: ");
-//        int principal = scanner.nextInt();
-//
-//        System.out.print("Annual Interest Rate: ");
-//        float annualInterestRate = scanner.nextFloat();
-//        float monthlyInterest = annualInterestRate / MONTHS_IN_YEAR / PERCENT;
-//
-//        System.out.print("Period (Years): ");
-//        byte years = scanner.nextByte();
-//        int noOfPeriod = years * MONTHS_IN_YEAR;
+        Scanner scanner = new Scanner(System.in);
 
-//        double mortgage = principal *
-//                (monthlyInterest * Math.pow((1+monthlyInterest), noOfPeriod)
-//                        / (Math.pow((1+monthlyInterest), noOfPeriod) - 1));
-//        String result = NumberFormat.getCurrencyInstance().format(mortgage);
-      System.out.println("Mortgage: " + result);
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        float annualInterestRate = scanner.nextFloat();
+        float monthlyInterest = annualInterestRate / MONTHS_IN_YEAR / PERCENT;
+
+        System.out.print("Period (Years): ");
+        byte years = scanner.nextByte();
+        int noOfPeriod = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal *
+                (monthlyInterest * Math.pow((1+monthlyInterest), noOfPeriod)
+                        / (Math.pow((1+monthlyInterest), noOfPeriod) - 1));
+        String result = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + result);
     }
 }
