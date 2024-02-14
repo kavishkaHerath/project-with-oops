00e21bf2f673820dee43db04454ec1a7e8931103
kavishkaHerath <hmravindu@gmail.com>
2024-02-14 09:04:30 +0530

    code clean

--- src/MortgageCalculator.java
+++ src/MortgageCalculator.java
@@ -1,6 +1,6 @@
 public class MortgageCalculator {
-    public final static byte MONTHS_IN_YEAR = 12;
-    public final static byte PERCENT = 100;
+    private final static byte MONTHS_IN_YEAR = 12;
+    private final static byte PERCENT = 100;
 
     private int principal;
     private float annualInterestRate;
@@ -28,14 +28,17 @@ public class MortgageCalculator {
                 * (Math.pow(1 + monthlyInterest, noOfPeriod) - Math.pow(1 + monthlyInterest, noOfPaymentMade))
                 / (Math.pow(1 + monthlyInterest, noOfPeriod) - 1));
     }
+    public double[] getRemainingBalance(){
+        double[] balances = new double[getNoOfPeriod()];
+        for (int month = 1; month <= balances.length; month++)
+            balances[month - 1] = calculateBalance(month);
+        return balances;
+    }
 
     private float getMonthlyInterest() {
         return annualInterestRate / MONTHS_IN_YEAR / PERCENT;
     }
     private int getNoOfPeriod() {
         return years * MONTHS_IN_YEAR;
-    }
-    public byte getYears() {
-        return years;
     }
 }
