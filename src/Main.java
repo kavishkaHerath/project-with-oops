d3a8b69ae4314130f4790cc7580b07ec723a1d7e
kavishkaHerath <hmravindu@gmail.com>
2024-02-14 07:49:14 +0530

    add console class

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 import java.text.NumberFormat;
-import java.util.Scanner;
 
 public class Main {
     final static byte MONTHS_IN_YEAR = 12;
     final static byte PERCENT = 100;
 
     public static void main(String[] args) {
-        int principal = (int) readNumber("Principal ($ 1k - $ 1M: ", 1000, 1_000_000);
-        float annualInterestRate = (float) readNumber("Annual Interest Rate: ", 1, 30);
-        byte years = (byte) readNumber("Period (Years): ", 1, 30);
+        int principal = (int) Console.readNumber("Principal ($ 1k - $ 1M: ", 1000, 1_000_000);
+        float annualInterestRate = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
+        byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
 
         double mortgage = calculateMortgage(principal, annualInterestRate, years);
 
@@ -35,18 +34,6 @@ public class Main {
         }
     }
 
-    public static double readNumber(String prompt, double min, double max){
-        Scanner scanner = new Scanner(System.in);
-        double value;
-        while (true){
-            System.out.print(prompt);
-            value = scanner.nextDouble();
-            if(value >= min && value <= max)
-                break;
-            System.out.println("Enter a value between " + min + " and " + max);
-        }
-        return value;
-    }
     public static double calculateMortgage(int principal, float annualInterestRate, byte years){
         float monthlyInterest = annualInterestRate / MONTHS_IN_YEAR / PERCENT;
 
